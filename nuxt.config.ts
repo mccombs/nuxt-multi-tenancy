8b33b23716a025461530e1575983a89134cdeda3
mccombs <amccombs@zello.com>
2024-08-31 18:27:53 -0500

    static = false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,16 @@
 export default defineNuxtConfig({
+  nitro: {
+    static: false,
+  },
   app: {
     head: {
-      title: 'Extending Configs is Fun!',
+      title: "Extending Configs is Fun!",
       meta: [
-        { name: 'description', content: 'I am using the extends feature in nuxt 3!' }
+        {
+          name: "description",
+          content: "I am using the extends feature in nuxt 3!",
+        },
       ],
-    }
-  }
-})
+    },
+  },
+});
